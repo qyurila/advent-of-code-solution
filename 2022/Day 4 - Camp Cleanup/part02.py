3f7701ad30038390510c215ca9b51f2b4aa9f346
QriLa <rea9lizer@gmail.com>
2022-12-04 14:21:31 +0900

    ✨ Add Python solution for Day 4

---
+++ 2022/Day 4 - Camp Cleanup/part02.py
@@ -0,0 +1,20 @@
+def solution():
+	counter = 0
+
+	for line in open(0):
+		pair = line.split(',')
+		elf1, elf2 = map(
+			lambda x: list(map(int, x.split('-'))),
+			pair
+		)
+
+		if (
+			elf1[1] >= elf2[0] and elf1[0] <= elf2[1]
+		) or (
+			elf1[1] <= elf2[0] and elf1[0] >= elf2[1]
+		):
+			counter += 1
+
+	return counter
+
+print(solution())

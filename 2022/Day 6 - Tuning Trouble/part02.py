b37a59c0621f148e253af2f988ec5bb0ee23ed74
QriLa <rea9lizer@gmail.com>
2022-12-06 15:41:48 +0900

    ✨ Add Python solution for Day 6

---
+++ 2022/Day 6 - Tuning Trouble/part02.py
@@ -0,0 +1,20 @@
+def solution():
+	buffer = open(0).readline()
+	scope_offset = 0
+	while True:
+		# Search for duplication
+		scope = buffer[scope_offset : scope_offset + 14]
+		for idx, char in enumerate(scope[:-1]):
+			if char in scope[idx + 1 :]:
+				# Make while loop continue if there is a duplication
+				break
+		else:
+			# Break while loop if there is no duplication
+			break
+
+		scope_offset += 1
+		continue
+
+	return scope_offset + 14
+
+print(solution())
